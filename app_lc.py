d2b9aba37f2c8a85a4cbec2287bc02f42dd37fa2
Mikhail Noskov <noskovmm@gmail.com>
2025-12-25 19:12:04 +0100

    main app updated

--- app_lc.py
+++ app_lc.py
@@ -8,6 +8,7 @@ import uuid
 from pythonjsonlogger.json import JsonFormatter
 from dotenv import load_dotenv
 
+from langchain_core.documents import Document
 from langchain_core.example_selectors import SemanticSimilarityExampleSelector
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder, PromptTemplate, FewShotPromptTemplate
 from langchain_core.chat_history import InMemoryChatMessageHistory
@@ -16,7 +17,7 @@ from langchain_community.vectorstores import Chroma
 from langchain_openai import ChatOpenAI
 from langchain_huggingface import HuggingFaceEmbeddings
 
-from schemas import StyleSchema
+from schemas import StyleSchema, ReplySchema
 
 __import__('pysqlite3')
 
@@ -56,16 +57,28 @@ def setup_jsonl_logger(session_id: uuid.UUID):
 
 class CliBot:
     def __init__(self, session_id):
+        print("Initializing Bot")
         self.chat_model = ChatOpenAI(
             model=os.getenv("MODEL_NAME", "gpt-5"),
             temperature=0,
             timeout=15
-        )
+        ).with_structured_output(ReplySchema)
         self.session_uuid = session_id
         self.history_store = {}
+        self._style = self._get_style()
+
+        self.embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+        self.example_selector = SemanticSimilarityExampleSelector.from_examples(
+            examples=self._get_samples(),
+            embeddings=self.embeddings,
+            vectorstore_cls=Chroma,
+            k=2
+        )
+        self.example_prompt = PromptTemplate.from_template("Вопрос: {question}\nОтвет: {answer}")
+        # self.faq_store = Chroma.from_documents(self._get_faq_docs(), self.embeddings)
+        # self.orders_store = Chroma.from_documents(self._get_orders_docs(), self.embeddings)
         self._faq = self._get_faq_info()
         self._orders = self._get_orders_info()
-        self._style = self._get_style()
         self.prompt = self._update_system_prompt()
         self.chain = self.prompt | self.chat_model
         self.chain_with_history = RunnableWithMessageHistory(
@@ -75,6 +88,7 @@ class CliBot:
             history_messages_key="history",
         )
         self.logger = setup_jsonl_logger(self.session_uuid)
+        print("Bot initialized")
 
     @classmethod
     def _get_style(cls):
@@ -85,18 +99,35 @@ class CliBot:
         orders_data_escaped = json.dumps(self._orders, ensure_ascii=False, indent=2).replace('{', '{{').replace('}',
                                                                                                                    '}}')
         system_prompt = f'''
-            {self._style.tone.role} бренда {self._style.brand}. Всегда сохраняй стиль ответа: {self._style.tone.persona}.
-            Отвечай не более {self._style.tone.sentences_max} предложений, избегая {self._style.tone.avoid} и включая
-            {self._style.tone.must_include}. Отвечай на {self._style.task}, используя правила: {self._style.rules}.
-            Ориентируйся при ответах на часто задаваемые вопросы {faq_data_escaped}, а при вопросах о заказах
-            ориентируйся на данные по заказам из {orders_data_escaped}
+            {self._style.tone.role} - {self._style.brand}. Always keep the reply style: {self._style.tone.persona}.
+            Not more than {self._style.tone.sentences_max} sentences, avoiding {self._style.tone.avoid} but including
+            {self._style.tone.must_include}. Reply to {self._style.task}, using the rules: {self._style.rules} strictly 
+            keeping the reply schema format{self._style.format}.
+            Take into account the context of faq from {faq_data_escaped} and of order info from {orders_data_escaped}
             '''
 
         return ChatPromptTemplate.from_messages([
             ("system", system_prompt),
             MessagesPlaceholder(variable_name="history"),
+            ("system", "{few_shots}"),
             ("human", "{question}"),
         ])
+
+    # def _update_system_prompt(self):
+    #     return ChatPromptTemplate.from_messages([
+    #         ("system", f"""
+    #         Ты {self._style.tone.role} бренда {self._style.brand}.
+    #         Всегда соблюдай стиль: {self._style.tone.persona}.
+    #         Максимум {self._style.tone.sentences_max} предложений.
+    #         Запрещено: {self._style.tone.avoid}.
+    #         Обязательно: {self._style.tone.must_include}.
+    #         Отвечай, используя правила: {self._style.rules} и структуру {ReplySchema}."""),
+    #         ("system", "Примеры правильных ответов:\n{few_shots}"),
+    #         ("system", "Часто задаваемые вопросы:\n{faq_context}"),
+    #         ("system", "Информация по заказам:\n{orders_context}"),
+    #         MessagesPlaceholder(variable_name="history"),
+    #         ("human", "{question}"),
+    #     ])
 
     def get_session_history(self, session_id: str):
         if session_id not in self.history_store:
@@ -104,7 +135,7 @@ class CliBot:
         return self.history_store[session_id]
 
     def __call__(self):
-        self.logger.info({"event": "session_start"})
+        self.logger.info({"event": f"session_start: {self.session_uuid}"})
         print(
             f"Чат-бот запущен {os.getenv('BRAND_NAME', 'No name')}! Можете задавать вопросы. \n - Для выхода введите 'выход'.\n - Для очистки контекста введите 'сброс'.\n")
         self.logger.info(f"=== New session {self.session_uuid}===")
@@ -188,39 +219,25 @@ class CliBot:
                     log_level=logging.ERROR
                 )
                 continue
-            bot_reply = response.content.strip()
-            response_meta = response.response_metadata.get("token_usage", None)
-            if response_meta is not None:
-                del response_meta["completion_tokens_details"]
-                del response_meta["prompt_tokens_details"]
             self._chat_and_log(
                 event="bot_reply",
-                msg=f"Бот: {bot_reply}",
-                usage=response_meta
+                msg=f"Бот: {response.answer}",
+                usage={
+                    "completion_tokens": response.usage.completion_tokens,
+                    "prompt_tokens": response.usage.prompt_tokens,
+                    "total_tokens": response.usage.total_tokens,
+                }
             )
 
-    def _get_llm_reply(self, text):
-        embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-        example_prompt = PromptTemplate.from_template("Вопрос: {question}\nОтвет: {answer}")
-        examples = self._get_samples()
-        example_selector = SemanticSimilarityExampleSelector.from_examples(
-            examples=examples,
-            embeddings=embeddings,
-            vectorstore_cls=Chroma,
-            k=2
+    def _get_llm_reply(self, text: str) -> ReplySchema:
+        few_shots = self._render_few_shots(text)
+        return self.chain_with_history.invoke(
+            {
+                "question": text,
+                "few_shots": few_shots,
+            },
+            {"configurable": {"session_id": self.session_uuid}},
         )
-        prompt = FewShotPromptTemplate(
-            example_selector=example_selector,
-            example_prompt=example_prompt,
-            suffix="Вопрос: {question}\nОтвет:",
-            input_variables=["question"]
-        )
-        formatted_prompt = prompt.format(question=text)
-        reply = self.chain_with_history.invoke(
-            {"question": formatted_prompt},
-            {"configurable": {"session_id": self.session_uuid}}
-        )
-        return reply
 
     def _chat_and_log(
             self,
@@ -237,7 +254,7 @@ class CliBot:
                 extra={"event": f"{event}", "usage": usage}
             )
         else:
-            print(msg or err_msg, file=sys.stderr)
+            print(msg)
             self.logger.error(
                 err_msg,
                 extra={"event": f"{event}"}
@@ -267,11 +284,48 @@ class CliBot:
             raise
         return user_text
 
+    def _render_few_shots(self, question: str) -> str:
+        few_shot_prompt = FewShotPromptTemplate(
+            example_selector=self.example_selector,
+            example_prompt=self.example_prompt,
+            prefix="",
+            suffix="",
+            input_variables=["question"],
+        )
+        return few_shot_prompt.format(question=question)
+        # examples = self.example_selector.select_examples({"question": question})
+        # print(examples)
+        # return "\n\n".join(
+        #     f"User: {ex['question']}\nAssistant: {ex['answer']}" if len(ex) > 0 else ""
+        #     for ex in examples
+        # )
+
+
+    # def _retrieve_faq(self, question: str) -> str:
+    #     docs = self.faq_store.similarity_search(question, k=3)
+    #     return "\n\n".join(d.page_content for d in docs)
+    #
+    # def _retrieve_orders(self, question: str) -> str:
+    #     docs = self.orders_store.similarity_search(question, k=1)
+    #     return "\n\n".join(d.page_content for d in docs)
+
+    def _get_faq_docs(self):
+        return [
+            Document(page_content=f"Q: {item['q']}\nA: {item['a']}")
+            for item in self._get_faq_info()
+        ]
+
     @classmethod
     def _get_faq_info(cls):
         with open('data/faq.json', 'r', encoding='utf-8') as f:
             faq_data_obj = json.load(f)
         return faq_data_obj
+
+    def _get_orders_docs(self):
+        return [
+            Document(page_content=f"{k}: {v}")
+            for k, v in self._get_orders_info().items()
+        ]
 
     @classmethod
     def _get_orders_info(cls):
